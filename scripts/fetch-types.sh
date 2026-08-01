12434c6953f8bfc90350832c3532fd1836a98987
水化 <168127639+suisanka@users.noreply.github.com>
2026-08-02 04:24:59 +0800

    chore: bump milky 1.3.0-rc.2

--- scripts/fetch-types.sh
+++ scripts/fetch-types.sh
@@ -3,8 +3,8 @@ rm -rf ./src/gen
 mkdir -p ./src/gen
 echo;
 echo 'Fetching types...'
-curl -o './src/gen/types.ts' 'https://milky.ntqqrev.org/raw/typescript/static.txt'
+curl -o './src/gen/types.ts' 'https://milky-next.ntqqrev.org/raw/typescript/static.txt'
 echo;
 echo 'Fetching zod types...'
-curl -o './src/gen/zod.ts' 'https://milky.ntqqrev.org/raw/typescript/zod.txt'
+curl -o './src/gen/zod.ts' 'https://milky-next.ntqqrev.org/raw/typescript/zod.txt'
 pnpm exec jiti ./scripts/generate-meta.ts
